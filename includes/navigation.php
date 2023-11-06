1a160a86cc98302b830afb4e742596c006897120
José María Gómez <gomezlobatojm@gmail.com>
2023-11-06 20:33:34 +0100

    fix (navbar)

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,39 +1,41 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark text-uppercase fixed-top" id="mainNav">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top p-0 m-0" id="mainNav">
 
     <div class="container-fluid">
         <div class="col-1 p-0">
-            <a class="navbar-brand" href="../index.php"><img src="../img/logos/SmallLogo.png" alt="Logo" width="60%"></a>
+            <a class="navbar-brand" href="../index.php"><img src="../img/logos/SmallLogo.png" alt="Logo"
+                    width="60%"></a>
         </div>
 
         <button class="navbar-toggler text-uppercase font-weight-bold text-white rounded" type="button"
             data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
             aria-expanded="false" aria-label="Toggle navigation">
 
-            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon "></span>
 
         </button>
 
-        <div class="container-fluid  navbar-collapse collapse col-10" id="navbarResponsive">
+        <div class="container-fluid  navbar-collapse collapse col-10 m-0 p-0 h-100 " id="navbarResponsive">
 
-            <ul class=" navbar-nav w-100 h-100 lobster">
+            <ul class=" navbar-nav w-100 h-100 lobster m-0">
 
-                <li class="nav-item mx-0 mx-lg-1 w-100 h-100 text-center"><a class="nav-link p-4"
-                        href="index.php">Reserva</a></li>
+                <li
+                    class="nav-item  w-100 h-100 text-center  ">
+                    <a class="nav-link p-4 " href="index.php">Reserva</a></li>
 
-                <li class="nav-item mx-0 mx-lg-1 w-100 h-100 text-center"><a
-                        class="nav-link p-4 border-end border-start border-light" href="about.php">Carta</a></li>
+                <li class="nav-item  w-100 h-100 text-center "><a
+                        class="nav-link p-4 " href="about.php">Carta</a></li>
 
-                <li class="nav-item mx-0 mx-lg-1 w-100 h-100 text-center"><a
-                        class="nav-link p-4 border-end border-light" href="contact.php">Sobre Nosotros</a></li>
+                <li class="nav-item  w-100 h-100 text-center "><a
+                        class="nav-link p-4" href="contact.php">Sobre<span id="invisible">_</span>Nosotros</a></li>
 
-                <li class="nav-item mx-0 mx-lg-1 w-100 h-100 text-center"><a class="nav-link p-4"
-                        href="contact.php">Ofertas</a></li>
+                <li class="nav-item  w-100 h-100 text-center "><a
+                        class="nav-link p-4 " href="contact.php">Ofertas</a></li>
 
-                <li class="nav-item mx-0 mx-lg-1 w-100 h-100 text-center"><a
-                        class="nav-link p-4 border-end border-start border-light" href="contact.php">Opiniones</a></li>
+                <li class="nav-item  w-100 h-100 text-center "><a
+                        class="nav-link p-4 " href="contact.php">Opiniones</a></li>
 
-                <li class="nav-item mx-0 mx-lg-1 w-100 h-100 text-center"><a class="nav-link p-4"
-                        href="../view/login.php">Identificarse</a></li>
+                <li class="nav-item  w-100 h-100 text-center  "><a
+                        class="nav-link p-4 " href="../view/login.php">Identificarse</a></li>
 
             </ul>
 
