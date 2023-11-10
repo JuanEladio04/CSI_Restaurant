caa6901b8ba519db1b29452df10ec3d27716b21f
Manuu500 <espartanmanu1122@gmail.com>
2023-11-10 12:50:43 +0100

    feat(aboutUs) Corrected the image size

--- view/aboutUs.php
+++ view/aboutUs.php
@@ -17,34 +17,33 @@
             =======
             >>>>>>> 64e70cb9ffd4aaeb593b33f1d008f3f4d5bf3940
             <div class="aboutus" id="background-<?php echo $CURRENT_PAGE; ?>">
-                <div class="banner7">
-                    <div class="bannercontent7 bg-dark text-white">
-                        <img class="logo-n1-1-icon7" alt="" src="./public/undefined.png" />
-                        <div class="container-flex options7 d-flex align-items-center">
-                            <div class="un-pequeo-vistazo2">Reservas</div>
-                            <div class="options-child32"></div>
-                            <div class="un-pequeo-vistazo2 margin-left-5">Carta</div>
-                            <div class="options-child32"></div>
-                            <div class="un-pequeo-vistazo2">Sobre Nosotros</div>
-                            <div class="options-child32"></div>
-                            <div class="un-pequeo-vistazo2">Ofertas</div>
-                            <div class="options-child32"></div>
-                            <div class="un-pequeo-vistazo2">Opiniones</div>
-                            <div class="options-child32"></div>
-                            <div class="un-pequeo-vistazo2">Identificarse</div>
+                <div>
+                    <div class="bg-dark text-white">
+                        <img alt="" src="./public/undefined.png" />
+                        <div class="container-flex d-flex align-items-center">
+                            <div>Reservas</div>
+                            <div></div>
+                            <div class="margin-left-5">Carta</div>
+                            <div></div>
+                            <div>Sobre Nosotros</div>
+                            <div></div>
+                            <div>Ofertas</div>
+                            <div></div>
+                            <div>Opiniones</div>
+                            <div></div>
+                            <div>Identificarse</div>
                         </div>
                     </div>
 
                     <div class="p-5 mt-5"></div>
 
 
-                    <div class="frame-parent1 mb-5 bg-secondary p-5 bg-info mt-5">
-                        <div class="un-pequeo-vistazo-parent">
-                            <div class="un-pequeo-vistazo fs-1 mb-5 text-center text-white">Sobre nosotros...</div>
-                            <div class="frame-child15"></div>
+                    <div class="mb-5 bg-secondary p-5 bg-info mt-5">
+                        <div>
+                            <div class="fs-1 mb-5 text-center text-white">Sobre nosotros...</div>
                         </div>
-                        <div class="lorem-ipsum-dolor-sit-amet-co-wrapper">
-                            <div class="lorem-ipsum-dolor1 fs-3 roboto text-white">
+                        <div>
+                            <div class="fs-3 roboto text-white">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
                                 ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -60,10 +59,10 @@
                 <div class="p-5 mt-3"></div>
 
 
-                <div class="row sobre-nosotros-parent bg-secondary mb-5 bg-danger">
-                    <div class="col sobre-nosotros8 fs-1 text-white p-5">
+                <div class="row bg-secondary mb-5 bg-danger">
+                    <div class="col fs-1 text-white p-5">
                         <p>Un pequeño vistazo</p>
-                        <p class="lorem-ipsum-dolor2 fs-3 mt-5 text-white roboto">
+                        <p class="fs-3 mt-5 text-white roboto">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                             enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -83,24 +82,23 @@
 
 
 
-                <div class="frame-parent2 mt-5">
-                <div class="row sobre-nosotros-parent bg-secondary mb-5 bg-danger">
-                <div class="col align-items-center text-white roboto position-relative bg-dark">
-                        <iframe class="position-absolute top-0 start-0 w-100 h-100 border-0"
-                            src="..\img\stockImages\aboutUs_StockImages\pexels-photo-8511864.jpeg" frameborder="0">
-                        </iframe>
+                <div class="mt-5">
+                    <div class="row bg-secondary mb-5 bg-danger">
+                        <div class="col align-items-center text-white roboto position-relative bg-dark">
+                            <img class="position-absolute align-self-center top-0 start-0 w-100 h-100 border-0"
+                                src="..\img\stockImages\aboutUs_StockImages\imagen-cocinero-aboutUs.jpg" frameborder="0">
+                            </img>
+                        </div>
+                        <div class="col fs-1 text-white p-5">
+                            <p>Un pequeño vistazo</p>
+                            <p class="fs-3 mt-5 text-white roboto">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                        </div>
                     </div>
-                    <div class="col sobre-nosotros8 fs-1 text-white p-5">
-                        <p>Un pequeño vistazo</p>
-                        <p class="lorem-ipsum-dolor2 fs-3 mt-5 text-white roboto">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                            enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                            nisi ut aliquip ex ea commodo consequat.
-                        </p>
-                    </div>
-                    
-                </div>
                 </div>
                 <div class="p-5 mt-5"></div>
 
