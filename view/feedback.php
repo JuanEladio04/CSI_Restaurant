a5e44411581efb01e81abeb5c5f17836d4823568
José María Gómez <gomezlobatojm@gmail.com>
2023-11-13 11:18:44 +0100

    fix(feedback)

--- view/feedback.php
+++ view/feedback.php
@@ -15,15 +15,20 @@
 </head>
 
 <body id="opinion">
-    <?php include("../includes/navigation.php"); ?>
+    <nav>
+        <?php include("../includes/navigation.php"); ?>
+    </nav>
 
     <!------------------------ Crear Comentario ------------------------>
-    <div class="container-fluid crearOpiniones">
-        <div class="titulo">
-            <h1 class="lobster">Deja tu opinion</h1>
-        </div>
-        <div class="contenedor">
+    <section class="container-fluid crearOpiniones">
+        <article>
+            <div class="titulo">
+                <h1 class="lobster">Deja tu opinion</h1>
+            </div>
+        </article>
+        <article class="contenedor">
             <div class="texto column p-5">
+
                 <div class="col-6 tu-opinin lobster">Tu opinión</div>
 
                 <div class="col-6 en-nuestro-restaurante-container">
@@ -64,27 +69,24 @@
                                 <i class="fa-regular fa-2x fa-star text-danger"></i>
                             </div>
                         </div>
-                        <div class="rectangle-parent7">
-                            <div class="frame-child14"></div>
-                            <div class="form-group shadow-textarea">
-                                <label class="roboto" id="label">Escriba su comentario:</label>
-                                <textarea class="form-control z-depth-1" id="textarea" rows="4"
-                                    placeholder=""></textarea>
-                            </div>
+                        <div class="form-group shadow-textarea">
+                            <label class="roboto" id="label">Escriba su comentario:</label>
+                            <textarea class="form-control z-depth-1" id="textarea" rows="4"></textarea>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <!------------------------ Comentarios Usuarios  ------------------------>
-    <div class="opinionUsuario ">
-        <div class="titulo">
-            <h1 class="lobster">Opiniones de Nuestros Clientes</h1>
-        </div>
+        </article>
+    </section>
+    <section class="opinionUsuario ">
+        <article>
+            <div class="titulo">
+                <h1 class="lobster title">Opiniones de Nuestros Clientes</h1>
+            </div>
+        </article>
         <div class="opinionesclientes p-5">
             <div class="frame-interior">
-                <div class="comentarybox1">
+                <article class="comentarybox1">
                     <div class="comment11">
                         <div class="arriba">
                             <img class="vector-icon img-user rounded" alt=""
@@ -112,7 +114,7 @@
                             Lorem Ipsum es simplemente el texto de relleno de las
                             imprentas y archivos de texto. Lorem Ipsum ha sido el texto de
                             relleno estándar de las industrias desde el año 1500, cuando
-                            <span class="span"> </span>un impresor (N. del T. persona que
+                            un impresor (N. del T. persona que
                             se dedica a la imprenta) desconocido usó una galería de textos
                             y los mezcló de tal manera que logró hacer un libro de textos
                             especimen. No sólo sobrevivió 500 años, sino que tambien
@@ -124,50 +126,9 @@
                             cual incluye versiones de Lorem Ipsum.
                         </div>
                     </div>
-                </div>
-                <div class="comentarybox2">
-                    <div class="comment12">
-                        <div class="arriba">
-                            <img class="vector-icon img-user rounded" alt=""
-                                src="../img/stockImages/defaultUserImage.png" />
-
-                            <div class="username lobster">Nombre de Usuario</div>
-
-                            <div class="stars">
-                                <i class="fa-solid fa-2x fa-star mx-auto text-danger"></i>
-
-                                <i class="fa-solid fa-2x fa-star mx-auto text-danger"></i>
-
-                                <i class="fa-solid fa-2x fa-star mx-auto text-danger"></i>
-
-                                <i class="fa-regular fa-2x fa-star text-danger"></i>
-
-                                <i class="fa-regular fa-2x fa-star text-danger"></i>
-
-                                <i class="fa-regular fa-2x fa-star text-danger"></i>
-
-                                <i class="fa-regular fa-2x fa-star text-danger"></i>
-                            </div>
-                        </div>
-                        <div class="comentario">
-                            Lorem Ipsum es simplemente el texto de relleno de las
-                            imprentas y archivos de texto. Lorem Ipsum ha sido el texto de
-                            relleno estándar de las industrias desde el año 1500, cuando
-                            <span class="span"> </span>un impresor (N. del T. persona que
-                            se dedica a la imprenta) desconocido usó una galería de textos
-                            y los mezcló de tal manera que logró hacer un libro de textos
-                            especimen. No sólo sobrevivió 500 años, sino que tambien
-                            ingresó como texto de relleno en documentos electrónicos,
-                            quedando esencialmente igual al original. Fue popularizado en
-                            los 60s con la creación de las hojas "Letraset", las cuales
-                            contenian pasajes de Lorem Ipsum, y más recientemente con
-                            software de autoedición, como por ejemplo Aldus PageMaker, el
-                            cual incluye versiones de Lorem Ipsum.
-                        </div>
-                    </div>
-                </div>
-                <div class="comentarybox3">
-                    <div class="comment13">
+                </article>
+                <article class="comentarybox2">
+                    <div class="comment11">
                         <div class="arriba">
                             <img class="vector-icon img-user rounded" alt=""
                                 src="../img/stockImages/defaultUserImage.png" />
@@ -183,7 +144,7 @@
 
                                 <i class="fa-solid fa-2x fa-star text-danger"></i>
 
-                                <i class="fa-regular fa-2x fa-star text-danger"></i>
+                                <i class="fa-solid fa-2x fa-star text-danger"></i>
 
                                 <i class="fa-regular fa-2x fa-star text-danger"></i>
 
@@ -194,7 +155,7 @@
                             Lorem Ipsum es simplemente el texto de relleno de las
                             imprentas y archivos de texto. Lorem Ipsum ha sido el texto de
                             relleno estándar de las industrias desde el año 1500, cuando
-                            <span class="span"> </span>un impresor (N. del T. persona que
+                            un impresor (N. del T. persona que
                             se dedica a la imprenta) desconocido usó una galería de textos
                             y los mezcló de tal manera que logró hacer un libro de textos
                             especimen. No sólo sobrevivió 500 años, sino que tambien
@@ -206,11 +167,51 @@
                             cual incluye versiones de Lorem Ipsum.
                         </div>
                     </div>
-                </div>
+                </article>
+                <article class="comentarybox3">
+                    <div class="comment11">
+                        <div class="arriba">
+                            <img class="vector-icon img-user rounded" alt=""
+                                src="../img/stockImages/defaultUserImage.png" />
+
+                            <div class="username lobster">Nombre de Usuario</div>
+
+                            <div class="stars">
+                                <i class="fa-solid fa-2x fa-star mx-auto text-danger"></i>
+
+                                <i class="fa-regular fa-2x fa-star text-danger"></i>
+
+                                <i class="fa-regular fa-2x fa-star text-danger"></i>
+
+                                <i class="fa-regular fa-2x fa-star text-danger"></i>
+
+                                <i class="fa-regular fa-2x fa-star text-danger"></i>
+
+                                <i class="fa-regular fa-2x fa-star text-danger"></i>
+
+                                <i class="fa-regular fa-2x fa-star text-danger"></i>
+                            </div>
+                        </div>
+                        <div class="comentario">
+                            Lorem Ipsum es simplemente el texto de relleno de las
+                            imprentas y archivos de texto. Lorem Ipsum ha sido el texto de
+                            relleno estándar de las industrias desde el año 1500, cuando
+                            un impresor (N. del T. persona que
+                            se dedica a la imprenta) desconocido usó una galería de textos
+                            y los mezcló de tal manera que logró hacer un libro de textos
+                            especimen. No sólo sobrevivió 500 años, sino que tambien
+                            ingresó como texto de relleno en documentos electrónicos,
+                            quedando esencialmente igual al original. Fue popularizado en
+                            los 60s con la creación de las hojas "Letraset", las cuales
+                            contenian pasajes de Lorem Ipsum, y más recientemente con
+                            software de autoedición, como por ejemplo Aldus PageMaker, el
+                            cual incluye versiones de Lorem Ipsum.
+                        </div>
+                    </div>
+                </article>
             </div>
         </div>
-    </div>
-
+    </section>
 
     <?php include("../includes/footer.php"); ?>
 
