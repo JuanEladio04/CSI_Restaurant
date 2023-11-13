4d3a73ad8fd0844e4d0b2625381061f89ba111c1
Isaac Delgado <113700281+Illu13@users.noreply.github.com>
2023-11-13 09:32:10 +0100

    fix(footer.php)

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
         <div class="row container-fluid">
             <!--Div del centro, en el que va el logo del restaurante y los logos de las redes sociales.-->
             <div class="col-sm-4 order-sm-2 d-flex flex-column align-items-center text-center">
-                <img src="../img/logos/BigLogo.png" class="imgFooter">
+                <img src="../img/logos/BigLogo.png">
                 <div class="d-flex mb-4 flex-row ">
                     <a href="#!" class="mx-4"><i class="fa-brands fa-instagram fa-2xl" style="color: #ff7b75;"></i></a>
                     <a href="#!" class="mx-4"><i class="fa-brands fa-x-twitter fa-2xl" style="color: #ff7b75;"></i></a>
