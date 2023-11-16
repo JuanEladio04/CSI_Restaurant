88d3c37ac495babbcf39f16154a2203bd34d0634
Isaac Delgado <113700281+Illu13@users.noreply.github.com>
2023-11-16 10:30:25 +0100

    fix(footer): minor changes Some changes to the margins of the footer

--- includes/footer.php
+++ includes/footer.php
@@ -11,11 +11,11 @@
                 </div>
             </div>
             <!--Div de la izquierda.-->
-            <div class="mx-auto col-sm-4 order-sm-1 mb-sm-5 my-auto">
-                <p class="lead mb-0 mb-2">
+            <div class="mx-auto col-sm-4 order-sm-1 my-sm-auto mb-2">
+                <p class="lead">
                     <a href="https://www.google.com/maps/place/C.+Juego+de+Pelota,+14900+Lucena,+C%C3%B3rdoba/data=!4m2!3m1!1s0xd6d7734a3a1046f:0x37967444a4027296?sa=X&ved=2ahUKEwi116fU1saCAxVT1AIHHSxoAiIQ8gF6BAg9EAA">C / Juego de Pelota</a>
                 </p>
-                <p class="lead mb-0 mb-2">
+                <p class="lead">
                     Nº 0 Datos
                 </p>
                 <p class="lead">
@@ -23,11 +23,11 @@
                 </p>
             </div>
             <!--Div en el que van los enlaces a Términos, avisos legales y políticas-->
-            <div class="mx-auto col-sm-4 order-sm-3 mb-sm-5 my-auto">
-                <p class="lead mb-2">
+            <div class="mx-auto col-sm-4 order-sm-3 my-sm-auto">
+                <p class="lead">
                     <a href="../view/terminosCondiciones.php">Términos y condiciones</a>
                 </p>
-                <p class="lead mb-2">
+                <p class="lead">
                     <a>Aviso legal</a>
                 </p>
                 <p class="lead">
