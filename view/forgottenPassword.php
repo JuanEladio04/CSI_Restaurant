e5848fc9d6d9231c2412fd0bcb636c96ebf0c331
Isaac Delgado <113700281+Illu13@users.noreply.github.com>
2023-11-17 11:12:59 +0100

    fix(view): major changes Carrousel eddited userGestion views have changed its structure

--- view/forgottenPassword.php
+++ view/forgottenPassword.php
@@ -9,59 +9,58 @@
 
 <body class="bg-dark" id="background-<?php echo $CURRENT_PAGE; ?>">
 
+    <header class="text-center">
+        <a href="../index.php"><img src="../img/logos/BigLogo.png" width="300px"></a>
+    </header>
+    <main class="container-fluid">
 
-    <section>
-        <div class="container-fluid align-items-center justify-content-center">
-            <div class="row">
+        <div class="row">
 
-                <!-- Main content container -->
-                <article class="col-12 d-flex flex-column align-items-center">
+            <!-- Main content container -->
 
-                    <!-- Restaurant logo -->
-                    <a href="../index.php"><img src="../img/logos/BigLogo.png" width="300px"></a>
+            <!-- Restaurant logo -->
 
-                    <!-- Form in a card -->
-                    <div class="container formUser">
-                        <div class="card bg-danger text-light roundedBorder col-lg-5 col-sm-4 ">
-                            <div class="card-header">
-                                <h1 class="text-center textoNoWrap">Contraseña olvidada</h1>
-                            </div>
-                            <div class="card-body my-auto">
-                                <div class="mt-3">
-                                    <!-- Form -->
-                                    <form action="/action_page.php">
-                                        <!-- Email input -->
-                                        <div class="margenInferior">
-                                            <label for="Email" class="form-label">Correo electrónico</label>
-                                            <input type="email" class="roundedInput form-control" id="email"
-                                                name="Email" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-                                                required>
-                                        </div>
-                                </div>
-                            </div>
+            <!-- Form in a card -->
+            <article class="col d-flex justify-content-center align-items-center">
 
-                            <!-- Footer with links to login and registration -->
-                            <div class="card-footer text-center">
-                                <a href="register.php" class="nav-link text-light col-sm-12 text-center">No tengo
-                                    cuenta</a>
-                                <div class="row">
-                                    <div class="col-sm-12 text-center">
-                                        <a href="login.php" class="nav-link text-light textoNoWrap">Identificarse</a>
+                <div class="formUser">
+                    <div class="card bg-danger text-light roundedBorder">
+                        <div class="card-header">
+                            <h1 class="text-center textoNoWrap">Contraseña olvidada</h1>
+                        </div>
+                        <div class="card-body my-auto">
+                            <div class="mt-3">
+                                <!-- Form -->
+                                <form action="/action_page.php">
+                                    <!-- Email input -->
+                                    <div class="margenInferior">
+                                        <label for="Email" class="form-label">Correo electrónico</label>
+                                        <input type="email" class="roundedInput form-control" id="email" name="Email"
+                                            pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required>
                                     </div>
-                                    <div class="col-sm-12 text-center">
-                                        <input type="submit" class="btn btn-primary roundedInput" name="enviarCorreo" value="Enviar correo">
-                                    </div>
-                                    </form>
-
-                                </div>
                             </div>
                         </div>
-                    </div>
-                </article>
 
-            </div>
+                        <!-- Footer with links to login and registration -->
+                        <div class="card-footer text-center">
+                            <a href="register.php" class="nav-link text-light col-sm-12 text-center">No tengo
+                                cuenta</a>
+                            <div class="text-center">
+                                <a href="login.php" class="nav-link text-light textoNoWrap">Identificarse</a>
+                            </div>
+                            <div class="text-center">
+                                <input type="submit" class="btn btn-primary roundedInput" name="enviarCorreo"
+                                    value="Enviar correo">
+                            </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </article>
+
         </div>
-    </section>
+    </main>
 
     <footer>
         <?php include("../includes/footer.php"); ?>
--- view/login.php
+++ view/login.php
@@ -9,65 +9,63 @@
 
 <body class="bg-dark" id="background-<?php echo $CURRENT_PAGE; ?>">
 
+    <header class="col-12 text-center">
+        <a href="../index.php"><img src="../img/logos/BigLogo.png" width="300px"></a>
+    </header>
 
-    <section>
-        <div class="container-fluid">
-            <div class="row">
 
-                <!-- Main content container -->
-                <article class="col-12 d-flex flex-column align-items-center">
+    <main class="container-fluid">
+        <div class="row">
 
-                    <!-- Restaurant logo -->
-                    <a href="../index.php"><img src="../img/logos/BigLogo.png" width="300px"></a>
+            <!-- Main content container -->
 
-                    <!-- Form in a card -->
-                    <div class="container formUser">
-                        <div class="card bg-danger text-light roundedBorder col-lg-5 col-sm-4">
-                            <div class="card-header">
-                                <h1 class="text-center">Identificarse</h1>
-                            </div>
-                            <div class="card-body">
-                                <div class="">
-                                    <!-- Form -->
-                                    <form action="/action_page.php" method="post">
-                                        <!-- Email input -->
-                                        <div class="margenInferior">
-                                            <label for="Email" class="form-label">Correo electrónico</label>
-                                            <input type="Email" class="roundedInput form-control" id="pwd" name="Email"
-                                                pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required>
-                                        </div>
-                                        <!-- Password input -->
-                                        <div class="col margenInferior">
-                                            <label for="Password1" class="form-label">Contraseña</label>
-                                            <input type="password" class="roundedInput form-control" required>
-                                        </div>
-                                </div>
-                            </div>
+            <!-- Restaurant logo -->
 
-                            <!-- Footer with links and login button -->
-                            <div class="card-footer text-center">
-                                <a href="forgottenPassword.php"
-                                    class="nav-link text-light col-sm-12 text-center">Contraseña olvidada</a>
-                                <div class="row">
-                                    <div class="col-sm-12 text-center ">
-                                        <a href="register.php" class="nav-link text-light textoNoWrap">No estoy
-                                            registrado</a>
+            <!-- Form in a card -->
+            <article class="col d-flex justify-content-center align-items-center">
+
+                <div class="container formUser">
+                    <div class="card bg-danger text-light roundedBorder col-lg-5 col-sm-4">
+                        <div class="card-header">
+                            <h1 class="text-center">Identificarse</h1>
+                        </div>
+                        <div class="card-body">
+                            <div class="">
+                                <!-- Form -->
+                                <form action="/action_page.php" method="post">
+                                    <!-- Email input -->
+                                    <div class="margenInferior">
+                                        <label for="Email" class="form-label">Correo electrónico</label>
+                                        <input type="Email" class="roundedInput form-control" id="pwd" name="Email"
+                                            pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required>
                                     </div>
-                                    <div class="col-sm-12 text-center">
-                                        <input type="submit"
-                                            class="btn btn-primary roundedInput" value="Identificarse">
+                                    <!-- Password input -->
+                                    <div class="col margenInferior">
+                                        <label for="Password1" class="form-label">Contraseña</label>
+                                        <input type="password" class="roundedInput form-control" required>
                                     </div>
-                                </div>
-                                </form>
-
                             </div>
                         </div>
-                    </div>
-                </article>
+                        <!-- Footer with links and login button -->
+                        <div class="card-footer text-center">
+                            <a href="forgottenPassword.php" class="nav-link text-light col-sm-12 text-center">Contraseña
+                                olvidada</a>
+                            <div class="text-center ">
+                                <a href="register.php" class="nav-link text-light textoNoWrap">No estoy
+                                    registrado</a>
+                            </div>
+                            <div class="text-center">
+                                <input type="submit" class="btn btn-primary roundedInput" value="Identificarse">
+                            </div>
+                            </form>
 
-            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
         </div>
-    </section>
+    </main>
 
     <?php include("../includes/footer.php"); ?>
 
