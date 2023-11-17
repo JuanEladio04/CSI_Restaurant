473751fc7ca83a80d4914167e5b562e0abd51306
JuanEladio04 <juaneladiopm@gmail.com>
2023-11-17 13:16:08 +0100

    feat(card) some improvement has been made

--- view/card.php
+++ view/card.php
@@ -12,9 +12,8 @@
 
     <!--Carta-->
     <section class=" cardMenu">
-        <h2 class="d-none">Menú de la carta</h2>
-        <!--PC menu card-->
-        <article>
+        <!--PC menu card------------------------------------------------>
+        <article class="d-lg-block d-none">
             <div class="container-fluid">
                 <div class="row bg-danger text-light justify-content-around">
                     <div class="col-2 menuSection">
@@ -51,6 +50,62 @@
             </div>
         </article>
 
+        <!--small devices menu------------------------------------------------>
+        <article class="container-fluid">
+            <div class="dropdown row d-lg-none d-block">
+                <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    Carta
+                </button>
+                <ul class="dropdown-menu bg-info text-center text-light" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item text-light p-2" href="#">Entrantes</a></li>
+                    <li><a class="dropdown-item text-light p-2" href="#">Ensaladas</a></li>
+                    <li><a class="dropdown-item text-light p-2" href="#">Pizzas</a></li>
+                    <li><a class="dropdown-item text-light p-2" href="#">Pastas</a></li>
+                    <li><a class="dropdown-item text-light p-2" href="#">Carnes</a></li>
+                    <li><a class="dropdown-item text-light p-2" href="#">Postres</a></li>
+                </ul>
+            </div>
+        </article>
+
+        <!--Card content------------------------------------------------>
+
+        <article class="container-fluid bg-primary">
+            <div class="row p-4 justify-content-around">
+
+                <div class="row bg-info col-3 m-2">
+                    <div class="col-8 p-3">
+                        <h5 class="card-title">Plato</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <img class="col-3 rounded-right m-0 p-0" src="../img/backgrounds/aboutUsIndex.png" alt="Card image cap">
+                </div>
+
+                <div class="row bg-info col-3 m-2 p-0">
+                    <div class="col-8 p-3">
+                        <h5 class="card-title">Plato</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <img class="col-3" src="..." alt="Card image cap">
+                </div>
+
+                <div class="row bg-info col-3 m-2">
+                    <div class="col-8 p-3">
+                        <h5 class="card-title">Plato</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <img class="col-3" src="..." alt="Card image cap">
+                </div>
+
+                <div class="row bg-info col-3 m-2">
+                    <div class="col-8 p-3">
+                        <h5 class="card-title">Plato</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <img class="col-3" src="..." alt="Card image cap">
+                </div>
+
+            </div>
+        </article>
     </section>
 
 
