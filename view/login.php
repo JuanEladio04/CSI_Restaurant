a3a43b635c4c1ae6ec992260c09aa07a31a2a224
JuanEladio04 <juaneladiopm@gmail.com>
2024-01-15 19:27:00 +0100

    feat(login and register) External login or registration buttons has been improved.

--- view/login.php
+++ view/login.php
@@ -3,6 +3,7 @@
 include("../includes/a_config.php");
 
 require_once "../controller/sessionController.php";
+
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 if (isset($_SESSION["usuario"])) {
@@ -30,12 +31,11 @@ if (isset($_POST['identificarse'])) {
             header('location: ../index.php');
         } else {
             $error = "El correo electrónico o la contraseña es incorrecta";
-
         }
     }
 }
 if (isset($_POST["gIdentify"])) {
-  header('location:'.$google_client->createAuthUrl());
+    header('location:' . $google_client->createAuthUrl());
 }
 
 
@@ -45,9 +45,7 @@ if (isset($_POST["gIdentify"])) {
 
 <head>
     <?php include("../includes/head-tag-contents.php"); ?>
-    <script async defer crossorigin="anonymous"
-        src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v18.0&appId=351483897629787"
-        nonce="zVvRb6qN"></script>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v18.0&appId=351483897629787" nonce="zVvRb6qN"></script>
 </head>
 
 <body class="bg-dark" id="background-<?php echo $CURRENT_PAGE; ?>">
@@ -78,60 +76,63 @@ if (isset($_POST["gIdentify"])) {
                             <h1 class="text-center">Identificarse</h1>
                         </div>
                         <div class="card-body">
-                            <!-- Form -->
-                            <form action="" method="POST">
-                                <!-- Email input -->
-                                <div class="margenInferior">
-                                    <label for="Email" class="form-label">Correo electrónico</label>
-                                    <input type="Email" class="roundedInput form-control" id="pwd" name="Email"
-                                        pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required>
-                                </div>
-                                <!-- Password input -->
-                                <div class="col margenInferior">
-                                    <label for="Password1" class="form-label">Contraseña</label>
-                                    <input type="password" name="password" class="roundedInput form-control" required>
-                                </div>
+                            <!--Inicios de sesión externos----------------------------------------------------------------------->
+                            <form action="" method="POST" class="container-fluid">
+                                <div class="fb-login-button col-3" data-width="400" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="false" onlogin="checkLoginState()"></div>
+
+                                <div class="row justify-content-around">
+                                    <!--Facebook-->
+                                    <button class="col-2 border-secondary bg-primary fb-login-button" data-width="400" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="false" onclick="checkLoginState()">
+                                        <img class="img-fluid" src="/img/stockImages/login_register/facebookLogo.png" alt="">
+                                    </button>
+                                    <!--X-->
+                                    <button class="col-2 border-secondary bg-primary"><img class="img-fluid" src="/img/stockImages/login_register/xLogo.png" alt=""></button>
+
+                                    <!--Google-->
+                                    <button class="col-2 border-secondary bg-primary" type="submit" name="gIdentify"><img class="img-fluid" src="/img/stockImages/login_register/googleLogo.png" alt=""></button>
+                            </form>
                         </div>
 
-                        <!-- Footer with links and login button -->
-                        <div class="card-footer text-center">
-                            <a href="forgottenPassword.php" class="nav-link text-light col-sm-12 text-center">Contraseña
-                                olvidada</a>
-                            <div class="text-center ">
-                                <a href="register.php" class="nav-link text-light textoNoWrap">No estoy
-                                    registrado</a>
-                                <div id="fb-root"></div>
-
-
-
+                        <!-- Form ---------------------------------------------------------------------------------------------->
+                        <form action="" method="POST">
+                            <!-- Email input -->
+                            <div class="margenInferior">
+                                <label for="Email" class="form-label">Correo electrónico</label>
+                                <input type="Email" class="roundedInput form-control" id="pwd" name="Email" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required>
                             </div>
-
-                            <div class="text-center">
-                                <?php
-                                if (isset($error)) {
-                                    print $error;
-                                }
-                                ?>
-                                <input type="submit" class="btn btn-primary roundedInput" name="identificarse"
-                                    value="Identificarse">
+                            <!-- Password input -->
+                            <div class="col margenInferior">
+                                <label for="Password1" class="form-label">Contraseña</label>
+                                <input type="password" name="password" class="roundedInput form-control" required>
                             </div>
-                            </form>
+                    </div>
 
-                            <div class="fb-login-button" data-width="400" data-size="" data-button-type=""
-                                data-layout="" data-auto-logout-link="false" data-use-continue-as="false"
-                                onlogin="checkLoginState()"></div>
-                            <form action="" method="POST">
-                                <input type="submit" class="btn btn-dark" value="Identificarse con X" name="xIdentify">
-                                <button type="submit" name="gIdentify"><img
-                                        src="../img/stockImages/google/sign-in-with-google.png" width="200px"
-                                        alt=""></button>
+                    <!-- Footer with links and login button ---------------------------------------------------------------------------------------->
+                    <div class="card-footer text-center">
+                        <a href="forgottenPassword.php" class="nav-link text-light col-sm-12 text-center">Contraseña
+                            olvidada</a>
+                        <div class="text-center ">
+                            <a href="register.php" class="nav-link text-light textoNoWrap">No estoy
+                                registrado</a>
+                            <div id="fb-root"></div>
 
-                            </form>
 
 
                         </div>
+
+                        <div class="text-center">
+                            <?php
+                            if (isset($error)) {
+                                print $error;
+                            }
+                            ?>
+                            <input type="submit" class="btn btn-primary roundedInput" name="identificarse" value="Identificarse">
+                        </div>
+                        </form>
+
                     </div>
                 </div>
+        </div>
         </div>
 
         </div>
