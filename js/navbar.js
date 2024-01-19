8b8f8f33f97b55a0a806842ece487621f835d6a2
José María Gómez <gomezlobatojm@gmail.com>
2024-01-19 12:26:07 +0100

    fix (navbar)

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,3 @@
-// Asignar el evento directamente al botón o elemento que activa la función mostrar
-document.getElementById('desplegable').addEventListener('click', mostrar);
-
-// Asignar el evento directamente al botón o elemento que activa la función mostrarSalir
-// document.getElementById('botonSalir').addEventListener('click', function (event) {
-//     event.preventDefault();
-//     mostrarSalir();
-// });
-
 function mostrar(){
     let menu = document.querySelector('.dropdown-menu');
     let estilo = window.getComputedStyle(menu);
@@ -20,19 +11,4 @@ function mostrar(){
 
 }
 
-// function mostrarSalir(){
-//     event.preventDefault();
-
-//     let cuadro = document.querySelector('.salir');
-//     let botonSalir = document.getElementById('botonSalir');
-
-//     if(cuadro.style.display === 'none'){
-//         cuadro.style.display = 'block';
-//         botonSalir.classList.remove('colorFondoClick');
-//         botonSalir.classList.add('colorFondo');
-//     }else{
-//         cuadro.style.display = 'none';
-//         botonSalir.classList.remove('colorFondo');
-//         botonSalir.classList.add('colorFondoClick');
-//     }
-// }
+// 
