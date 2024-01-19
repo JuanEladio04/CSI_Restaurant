27ddb5112e5781006ccb4c9f8250fac15bb73671
JuanEladio04 <juaneladiopm@gmail.com>
2024-01-19 12:47:25 +0100

    feat(feedBack) Added text editor to the comentary box

--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -19,5 +19,37 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
 <link href="/css/styles.css" rel="stylesheet" />
-	
+
+<?php
+if ($CURRENT_PAGE == 'feedback') {
+?>
+
+	<meta name="description" content="Quill is a free, open source WYSIWYG editor built for the modern web. Completely customize it for any need with its modular architecture and expressive API.">
+
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@quilljs">
+
+	<meta name="twitter:title" content="Full Editor - Quill">
+
+	<meta name="twitter:description" content="Quill is a free, open source rich text editor built for the modern web.">
+	<meta name="twitter:image" content="https://quilljs.com/assets/images/brand-asset.png">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://quilljs.com/standalone/full/">
+	<meta property="og:image" content="https://quilljs.com/assets/images/brand-asset.png">
+	<meta property="og:title" content="Full Editor - Quill">
+	<meta property="og:site_name" content="Quill">
+
+	<link rel="canonical" href="https://quilljs.com/standalone/full/">
+	<link type="application/atom+xml" rel="alternate" href="https://quilljs.com/feed.xml" title="Quill - Your powerful rich text editor" />
+
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css" />
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/monokai-sublime.min.css" />
+<?php
+}
+?>
+
+
+
 <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
--- view/feedback.php
+++ view/feedback.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 include("../includes/a_config.php");
 require_once('../controller/sessionController.php');
- ?>
+?>
 
 <!DOCTYPE html>
 
@@ -18,65 +18,131 @@ require_once('../controller/sessionController.php');
     <!--CF: Error grave, la página carece de una estructura row/col-->
     <section class="container-fluid crearOpiniones">
         <div class="row">
-        <div class="titulo">
-            <h2>Deja tu opinión</h2>
-        </div>
+            <div class="titulo">
+                <h2>Deja tu opinión</h2>
+            </div>
 
 
-        <article class="contenedor">
-            <div class="texto column p-5 tituloh2">
+            <article class="contenedor">
+                <div class="texto column p-5 tituloh2">
 
-                <h3 class="col-6 opinion ">Tu opinión</h3>
+                    <h3 class="col-6 opinion ">Tu opinión</h3>
 
-                <div class="col-6 parrafo-container p">
+                    <div class="col-6 parrafo-container p">
 
-                    <p class="parrafo ">
-                        En nuestro restaurante tu voz importa.
-                    </p>
+                        <p class="parrafo ">
+                            En nuestro restaurante tu voz importa.
+                        </p>
 
-                    <p class="parrafo ">¿Quieres hablar?</p>
+                        <p class="parrafo ">¿Quieres hablar?</p>
 
-                    <p class="parrafo roboto">
-                        Deja tu opinión a continuación:
-                    </p>
+                        <p class="parrafo roboto">
+                            Deja tu opinión a continuación:
+                        </p>
 
-                </div>
+                    </div>
 
-                <div class="comentarybox">
-                    <div>
-                        <div class="arriba mt-4 mb-2">
-                            <div class="img">
-                                <img class="vector-icon img-user rounded" alt=""
-                                    src="../img/stockImages/defaultUserImage.png" />
-                            </div>
-
-                            <div class="username">Nombre de Usuario</div>
-
-                            <div id="crearCalificacion" class="stars">
-                                <i class="fa-regular fa-2x fa-star text-danger estrella" id="1"></i>
-
-                                <i class="fa-regular fa-2x fa-star text-danger estrella" id="2"></i>
-
-                                <i class="fa-regular fa-2x fa-star text-danger estrella" id="3"></i>
-
-                                <i class="fa-regular fa-2x fa-star text-danger estrella" id="4"></i>
-
-                                <i class="fa-regular fa-2x fa-star text-danger estrella" id="5"></i>
-                            </div>
-                        </div>
-                        <div class="crearComentario">
-                            <label class="roboto" id="label">Escriba su comentario:</label>
-                            <form>
-                                <textarea class="form-control z-depth-1" id="textarea" rows="4"></textarea>
-                                <div id="boton">
-                                    <button type="button" class="btn btn-danger">Enviar</button>
+                    <div class="comentarybox">
+                        <div>
+                            <div class="arriba mt-4 mb-2">
+                                <div class="img">
+                                    <img class="vector-icon img-user rounded" alt="" src="../img/stockImages/defaultUserImage.png" />
                                 </div>
-                            </form>
+
+                                <div class="username">Nombre de Usuario</div>
+
+                                <div id="crearCalificacion" class="stars">
+                                    <i class="fa-regular fa-2x fa-star text-danger estrella" id="1"></i>
+
+                                    <i class="fa-regular fa-2x fa-star text-danger estrella" id="2"></i>
+
+                                    <i class="fa-regular fa-2x fa-star text-danger estrella" id="3"></i>
+
+                                    <i class="fa-regular fa-2x fa-star text-danger estrella" id="4"></i>
+
+                                    <i class="fa-regular fa-2x fa-star text-danger estrella" id="5"></i>
+                                </div>
+                            </div>
+
+                            <div id="standalone-container" class="crearComentario">
+                                <div id="toolbar-container">
+                                    <span class="ql-formats">
+                                        <select class="ql-font"></select>
+                                        <select class="ql-size"></select>
+                                    </span>
+                                    <span class="ql-formats">
+                                        <button class="ql-bold"></button>
+                                        <button class="ql-italic"></button>
+                                        <button class="ql-underline"></button>
+                                        <button class="ql-strike"></button>
+                                    </span>
+                                    <span class="ql-formats">
+                                        <select class="ql-color"></select>
+                                        <select class="ql-background"></select>
+                                    </span>
+                                    <span class="ql-formats">
+                                        <button class="ql-script" value="sub"></button>
+                                        <button class="ql-script" value="super"></button>
+                                    </span>
+                                    <span class="ql-formats">
+                                        <button class="ql-header" value="1"></button>
+                                        <button class="ql-header" value="2"></button>
+                                        <button class="ql-blockquote"></button>
+                                        <button class="ql-code-block"></button>
+                                    </span>
+                                    <span class="ql-formats">
+                                        <button class="ql-list" value="ordered"></button>
+                                        <button class="ql-list" value="bullet"></button>
+                                        <button class="ql-indent" value="-1"></button>
+                                        <button class="ql-indent" value="+1"></button>
+                                    </span>
+                                    <span class="ql-formats">
+                                        <button class="ql-direction" value="rtl"></button>
+                                        <select class="ql-align"></select>
+                                    </span>
+                                    <span class="ql-formats">
+                                        <button class="ql-link"></button>
+                                        <button class="ql-image"></button>
+                                        <button class="ql-video"></button>
+                                        <button class="ql-formula"></button>
+                                    </span>
+                                    <span class="ql-formats">
+                                        <button class="ql-clean"></button>
+                                    </span>
+                                </div>
+                                <div id="editor-container"></div>
+                            </div>
+
+
+
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.js"></script>
+
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+
+                            <script src="../js/editorDeTexto/quill.min.js"></script>
+
+                            <script>
+                                var quill = new Quill('#editor-container', {
+                                    modules: {
+                                        formula: true,
+                                        syntax: true,
+                                        toolbar: '#toolbar-container'
+                                    },
+                                    placeholder: 'Escriba aqui su comentario...',
+                                    theme: 'snow'
+                                });
+                            </script>
+
+                            <div style="width: 100%; text-align: center;">
+                                <button type="button" class="btn btn-primary btn-sm" aria-label="Close" onclick="JavaScript: alert(quill.root.innerHTML);">
+                                    Obtener texto HTML mostrado en el editor
+                                </button>
+                            </div>
+
                         </div>
                     </div>
                 </div>
-            </div>
-        </article>
+            </article>
         </div>
     </section>
     <section class="opinionUsuario ">
@@ -89,8 +155,7 @@ require_once('../controller/sessionController.php');
                     <div>
                         <div class="arriba">
                             <div class="img">
-                                <img class="vector-icon img-user rounded" alt=""
-                                    src="../img/stockImages/defaultUserImage.png" />
+                                <img class="vector-icon img-user rounded" alt="" src="../img/stockImages/defaultUserImage.png" />
                             </div>
                             <div class="username lobster">Nombre de Usuario</div>
 
@@ -127,8 +192,7 @@ require_once('../controller/sessionController.php');
                     <div>
                         <div class="arriba">
                             <div class="img">
-                                <img class="vector-icon img-user rounded" alt=""
-                                    src="../img/stockImages/defaultUserImage.png" />
+                                <img class="vector-icon img-user rounded" alt="" src="../img/stockImages/defaultUserImage.png" />
                             </div>
 
                             <div class="username lobster">Nombre de Usuario</div>
@@ -166,8 +230,7 @@ require_once('../controller/sessionController.php');
                     <div>
                         <div class="arriba">
                             <div class="img">
-                                <img class="vector-icon img-user rounded" alt=""
-                                    src="../img/stockImages/defaultUserImage.png" />
+                                <img class="vector-icon img-user rounded" alt="" src="../img/stockImages/defaultUserImage.png" />
                             </div>
 
                             <div class="username lobster">Nombre de Usuario</div>
