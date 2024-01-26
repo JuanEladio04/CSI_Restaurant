e3c26e48ab5535c03d9f87f07e4afd3d0c16db16
Manuu500 <espartanmanu1122@gmail.com>
2024-01-26 10:51:07 +0100

    feat(Made the base of cookies)

--- includes/cookies.php
+++ includes/cookies.php
@@ -1,21 +1,17 @@
-<div class="overlay" id="ol"></div>
+<?php
+if($_COOKIE['cookiesAceptadas'] == null){
 
-<div class="container fs-4 cookies roundedBorder" role="alert" id="cookieAlert">
-    <p class="fs-4">Esta página web utiliza las <a href="termsAndConditions.php">cookies</a> para recordar las acciones del usuario</p>
-    <button type="button" class="button" id="enviar" onclick="desaparecerBanner()">
-        OK
-    </button>
-</div>
+    ?>
+    <div class="overlay" id="ol"></div>
+    <div class="container fs-4 cookies roundedBorder" role="alert" id="cookieAlert">
+        <p class="fs-4">Esta página web utiliza las <a href="termsAndConditions.php">cookies</a> para recordar las acciones del usuario</p>
+        <button type="button" class="button" id="enviar" onclick="desaparecerBanner()">
+            OK
+        </button>
+    </div>
 
-<script>
+<script src="../js/cookies.js"></script>
+    <?php
     
-
-    function desaparecerBanner(){
-        var enviar = document.getElementById("enviar");
-        var overlay = document.getElementById("ol");
-        var cookie = document.getElementById("cookieAlert");
-
-        overlay.style.display = "none";
-        cookie.style.display = "none";   
-    }
-</script>
+}
+?>
---
+++ js/cookies.js
@@ -0,0 +1,19 @@
+
+    function desaparecerBanner(){
+        var cookieAlert = document.getElementById("cookieAlert");
+        var overlay = document.getElementById("ol");
+        overlay.style.display = "none";
+        cookieAlert.style.display = "none";  
+    
+        setCookie("cookiesAceptadas", true, 365);
+        cookieAlert.style.display = "none";  
+         
+        function setCookie(cname, cvalue, exdays) {
+            const d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            let expires = "expires="+d.toUTCString();
+            document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        }
+    }
+
+
