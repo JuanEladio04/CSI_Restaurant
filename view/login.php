94c6560bf3ac4e8cbdfdbf0c0959d668a10b677f
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-02-02 12:09:53 +0100

    fix(login) External loggin buttons stetic has been fixed

--- view/login.php
+++ view/login.php
@@ -45,24 +45,23 @@ if (isset($_POST["gIdentify"])) {
 
 <head>
     <?php include("../includes/head-tag-contents.php"); ?>
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v18.0&appId=351483897629787" nonce="zVvRb6qN"></script>
+    <script async defer crossorigin="anonymous"
+        src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v18.0&appId=351483897629787"
+        nonce="zVvRb6qN"></script>
 </head>
 
 <body class="bg-dark" id="background-<?php echo $CURRENT_PAGE; ?>">
 
     <header class="col-12 text-center">
+        <!-- Restaurant logo -->
         <a href="../index.php"><img src="../img/logos/BigLogo.png" width="300px"></a>
     </header>
 
+    <!-- Main content container -->
 
     <main class="container-fluid">
         <div class="row">
 
-            <!-- Main content container -->
-
-            <!-- Restaurant logo -->
-
-            <!-- Form in a card -->
 
             <!--CF2: Esto no es un article, es un form-->
 
@@ -76,67 +75,74 @@ if (isset($_POST["gIdentify"])) {
                             <h1 class="text-center">Identificarse</h1>
                         </div>
                         <div class="card-body">
-                            <!--Inicios de sesión externos----------------------------------------------------------------------->
-                            <form action="" method="POST" class="container-fluid">
-                                <div class="fb-login-button col-3" data-width="400" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="false" onlogin="checkLoginState()"></div>
 
-                                <div class="row justify-content-around">
-                                    <!--Facebook-->
-                                    <button class="col-2 border-secondary bg-primary fb-login-button" data-width="400" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="false" onclick="checkLoginState()">
-                                        <img class="img-fluid" src="/img/stockImages/login_register/facebookLogo.png" alt="">
-                                    </button>
-                                    <!--X-->
-                                    <button name="xIdentify" class="col-2 border-secondary bg-primary"><img class="img-fluid" src="/img/stockImages/login_register/xLogo.png" alt=""></button>
+                            <!-- Form ---------------------------------------------------------------------------------------------->
+                            <form action="" method="POST">
+                                <!-- Email input -->
+                                <div class="margenInferior">
+                                    <label for="Email" class="form-label">Correo electrónico</label>
+                                    <input type="Email" class="roundedInput form-control" id="pwd" name="Email"
+                                        pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required>
+                                </div>
+                                <!-- Password input -->
+                                <div class="col margenInferior">
+                                    <label for="Password1" class="form-label">Contraseña</label>
+                                    <input type="password" name="password"
+                                        class="roundedInput form-control margenInferior" required>
+                                    <p class="text-center">
+                                        <?php
+                                        if (isset($error)) {
+                                            print "<span class='material-symbols-outlined'> warning </span>  " .
+                                                $error .
+                                                "  <span class='material-symbols-outlined'> warning </span>";
+                                        }
+                                        ?>
+                                    </p>
+                                </div>
+                        </div>
 
-                                    <!--Google-->
-                                    <button class="col-2 border-secondary bg-primary" type="submit" name="gIdentify"><img class="img-fluid" src="/img/stockImages/login_register/googleLogo.png" alt=""></button>
+                        <!-- Footer with links and login button ---------------------------------------------------------------------------------------->
+                        <div class="card-footer text-center">
+                            <a href="forgottenPassword.php" class="nav-link text-light col-sm-12 text-center">Contraseña
+                                olvidada</a>
+                            <div class="text-center ">
+                                <a href="register.php" class="nav-link text-light textoNoWrap">No estoy
+                                    registrado</a>
+                                <div id="fb-root"></div>
+                            </div>
+
+                            <div class="text-center">
+                                <input type="submit" class="btn btn-primary roundedInput" name="identificarse"
+                                    value="Identificarse">
+                            </div>
                             </form>
                         </div>
 
-                        <!-- Form ---------------------------------------------------------------------------------------------->
-                        <form action="" method="POST">
-                            <!-- Email input -->
-                            <div class="margenInferior">
-                                <label for="Email" class="form-label">Correo electrónico</label>
-                                <input type="Email" class="roundedInput form-control" id="pwd" name="Email" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required>
+                        <!-- External loggins ---------------------------------------------------------------------------------------->
+                        <form action="" method="POST" class="row justify-content-around card-footer text-center">
+                            <p>
+                                O inicia sesión con otra plataforma:
+                            </p>
+                            <!-- Facebook -->
+                            <div class="fb-login-button col-2 eLogin-button" data-width="400" data-size=""
+                                data-button-type="" data-layout="" data-auto-logout-link="false"
+                                data-use-continue-as="false" onlogin="checkLoginState()">
+                                
                             </div>
-                            <!-- Password input -->
-                            <div class="col margenInferior">
-                                <label for="Password1" class="form-label">Contraseña</label>
-                                <input type="password" name="password" class="roundedInput form-control margenInferior" required>
-                                <p class="text-center">
-                                    <?php
-                                    if (isset($error)) {
-                                        print "<span class='material-symbols-outlined'> warning </span>  " .
-                                            $error .
-                                            "  <span class='material-symbols-outlined'> warning </span>";
-                                    }
-                                    ?>
-                                </p>
-                            </div>
-                    </div>
 
-                    <!-- Footer with links and login button ---------------------------------------------------------------------------------------->
-                    <div class="card-footer text-center">
-                        <a href="forgottenPassword.php" class="nav-link text-light col-sm-12 text-center">Contraseña
-                            olvidada</a>
-                        <div class="text-center ">
-                            <a href="register.php" class="nav-link text-light textoNoWrap">No estoy
-                                registrado</a>
-                            <div id="fb-root"></div>
+                            <!-- Google -->
+                            <button class="col-2 border-secondary eLogin-button" type="submit" name="gIdentify">
+                                <img class="img-fluid" src="/img/stockImages/login_register/googleLogo.png" alt="">
+                            </button>
 
-
-
-                        </div>
-
-                        <div class="text-center">
-                            <input type="submit" class="btn btn-primary roundedInput" name="identificarse" value="Identificarse">
-                        </div>
+                            <!-- X -->
+                            <button name="xIdentify" class="col-2 eLogin-button">
+                                <img class="img-fluid" src="/img/stockImages/login_register/xLogo.png" alt="">
+                            </button>
                         </form>
 
                     </div>
                 </div>
-        </div>
         </div>
 
         </div>
