b1c7dfa1dfc9304d4790ca4db9d4765552ffcb31
JuanEladio04 <113886575+JuanEladio04@users.noreply.github.com>
2024-02-09 12:31:51 +0100

    fix(footer) Some accesibility ajustement has been made

--- includes/footer.php
+++ includes/footer.php
@@ -7,42 +7,54 @@
             <!-- Centered column for the restaurant logo and social media icons. -->
             <div class="col-sm-4 order-sm-2 d-flex flex-column align-items-center text-center">
                 <!-- Link to the home page with the restaurant logo. -->
-                <a href="/index.php"><img src="/img/logos/BigLogo.png" width="250vh"></a>
+                <a href="/index.php" class="my-3"><img class="d-block" src="/img/logos/BigLogo.png"
+                        alt="Logo principal del restaurante" width="250vh"> Ristorante Luna della Rossa</a>
                 <!-- Social media icons in a horizontal row. -->
                 <div class="d-flex mb-4 flex-row ">
-                    <a href="https://www.instagram.com/ristaurantelunadellarosa/" class="mx-4"><i class="fa-brands fa-instagram text-primary fa-2xl"></i></a>
-                    <a href="https://twitter.com/RistauranteDLR" class="mx-4"><i class="fa-brands fa-x-twitter text-primary fa-2xl"></i></a>
-                    <a href="https://www.facebook.com/profile.php?id=61553606555788" class="mx-4"><i class="fa-brands fa-facebook text-primary fa-2xl"></i></i></a>
+                    <a href="https://www.instagram.com/ristaurantelunadellarosa/" class="mx-4"><i
+                            class="fa-brands fa-instagram text-primary fa-2xl d-block my-3"></i> Instagram</a>
+                    <a href="https://twitter.com/RistauranteDLR" class="mx-4"><i
+                            class="fa-brands fa-x-twitter text-primary fa-2xl d-block my-3"></i> Twitter</a>
+                    <a href="https://www.facebook.com/profile.php?id=61553606555788" class="mx-4"><i
+                            class="fa-brands fa-facebook text-primary fa-2xl d-block my-3"></i> Facebook</a>
                 </div>
             </div>
             <!-- Left column with address, contact information, and order information. -->
             <div class="mx-auto col-sm-4 order-sm-1 my-sm-auto mb-2">
+                <h3>
+                    Datos de interés
+                </h3>
                 <p class="lead">
                     <!-- Link to Google Maps with the restaurant address. -->
-                    <a href="https://www.google.com/maps/place/C.+Juego+de+Pelota,+14900+Lucena,+C%C3%B3rdoba/data=!4m2!3m1!1s0xd6d7734a3a1046f:0x37967444a4027296?sa=X&ved=2ahUKEwi116fU1saCAxVT1AIHHSxoAiIQ8gF6BAg9EAA">C / Juego de Pelota</a>
+                    <a
+                        href="https://www.google.com/maps/place/C.+Juego+de+Pelota,+14900+Lucena,+C%C3%B3rdoba/data=!4m2!3m1!1s0xd6d7734a3a1046f:0x37967444a4027296?sa=X&ved=2ahUKEwi116fU1saCAxVT1AIHHSxoAiIQ8gF6BAg9EAA">Enlace
+                        a la dirección: C / Juego de Pelota</a>
                 </p>
                 <p class="lead">
                     <!-- Placeholder text for order information. -->
-                    Nº 0 Datos
+                    Número y local: Nº 0 Datos
                 </p>
                 <p class="lead">
                     <!-- Contact phone number. -->
-                    +34 333333333
+                    Número de telefono de contacto: +34 333333333
                 </p>
             </div>
             <!-- Right column for links to Terms, Legal Notice, and Privacy Policy. -->
             <div class="mx-auto col-sm-4 order-sm-3 my-sm-auto">
+                <h3>
+                    Apartado legal de la página
+                </h3>
                 <p class="lead">
                     <!-- Link to the Terms and Conditions page. -->
-                    <a href="/view/termsAndConditions.php">Términos y condiciones</a>
+                    <a href="/view/termsAndConditions.php">Ver términos y condiciones</a>
                 </p>
                 <p class="lead">
                     <!-- Placeholder link to Legal Notice. -->
-                    <a  href="/view/termsAndConditions.php#legalWarn">Aviso legal</a>
+                    <a href="/view/termsAndConditions.php#legalWarn">Ver aviso legal</a>
                 </p>
                 <p class="lead">
                     <!-- Placeholder link to Privacy Policy. -->
-                    <a  href="/view/termsAndConditions.php#privPol">Política de privacidad</a>
+                    <a href="/view/termsAndConditions.php#privPol">Ver política de privacidad</a>
                 </p>
             </div>
         </div>
